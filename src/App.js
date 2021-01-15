80111863c752ecd3589888079a5104e32ff84e08
eman1970 <tommy.hallqvist@gmail.com>
2021-01-15 01:06:49 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
  
- 
+ import React from 'react'
+ import {MyFirstComponent} from './components/myfirstcomponent/myfirstcomponent.js'
 
 function App() {
   return (
-     <div><h1>React Tutorial</h1></div>
+
+    <>
+     <h1>React Tutorial</h1>
+     
+< MyFirstComponent />
+< MyFirstComponent />
+
+</>
+
   );
 }
 
---
+++ src/components/myfirstcomponent/myfirstcomponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './myfirstcomponent.css'
+
+export const MyFirstComponent = () => {
+
+
+return (
+
+    <>
+    <h1 className="title">Min första react komponent</h1>
+
+</>
+)
+
+
+}
