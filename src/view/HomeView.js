5bdee20d85a32e1597d202fffdb9359da15d39ad
eman1970 <tommy.hallqvist@gmail.com>
2021-01-24 01:57:09 +0100

    Effect

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const HomeView = () => {
+    const [count, setcount] = useState(0)
+
+    useEffect(() => {
+
+
+        alert("Component is being rendered")
+        return () => {
+            alert("Component is being removed")
+
+
+        }
+    }, [count])
 
     return (
 
         <div>
-            <h1>This is the HomeView</h1>
-
+            <h1>{count}</h1>
+            <button onClick={() => setcount(count + 1)}>Increment with 1</button>
         </div>
 
 
